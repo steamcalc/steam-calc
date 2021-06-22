8e80309fcaeaa5ef8613999dda0468b316ca1553
mope10 <moperashid1@gmail.com>
2021-06-23 02:21:25 +0500

    created Vue App

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <the-header></the-header>
+  <router-view />
+</template>
+
+<script>
+import TheHeader from './components/UI/TheHeader.vue';
+export default {
+  components: {
+    TheHeader,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
