499f699c35e00ea84dbe5d4bf5ac0d948ee9bfc5
mope10 <moperashid1@gmail.com>
2021-06-25 12:05:11 +0500

    landing page components added

---
+++ src/components/home/InfoItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="item">
+    <img :src="require('@/assets/' + imgSrc)" />
+    <h2>{{ title }}</h2>
+    <p>{{ description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['imgSrc', 'title', 'description'],
+};
+</script>
+
+<style scoped>
+.item {
+  max-width: 15em;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 1rem 0;
+}
+img {
+  width: 8em;
+  height: 8em;
+}
+h2 {
+  margin: 0;
+}
+p {
+  margin: 0;
+  padding: 0;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,46 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img alt="Vue logo" src="@/assets/logo.png" />
+    <base-section title="Why Us?">
+      <div class="info-items">
+        <info-item
+          img-src="free.png"
+          title="Free"
+          description="know the value of your steam account completely free"
+        ></info-item>
+        <info-item
+          img-src="free.png"
+          title="No Login"
+          description="know the worth of your steam account completely free"
+        ></info-item>
+        <info-item
+          img-src="free.png"
+          title="Full Privacy"
+          description="know the worth of your steam account completely free"
+        ></info-item>
+      </div>
+    </base-section>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import BaseSection from '@/components/UI/BaseSection.vue';
+import InfoItem from '@/components/home/InfoItem.vue';
 
 export default {
-  name: "Home",
   components: {
-    HelloWorld,
+    BaseSection,
+    InfoItem,
   },
 };
 </script>
+
+<style scoped>
+.info-items {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+}
+</style>
