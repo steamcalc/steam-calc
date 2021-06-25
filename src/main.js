feb4bd3dfde59b8c3c1968c6497d6fe24327f95d
mope10 <moperashid1@gmail.com>
2021-06-25 12:05:37 +0500

    configured fontawesome

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
-createApp(App).use(store).use(router).mount("#app");
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faBars);
+
+createApp(App)
+  .use(store)
+  .use(router)
+  .component('font-awesome-icon', FontAwesomeIcon)
+  .mount('#app');
